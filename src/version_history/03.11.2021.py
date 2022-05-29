6dee4b2b24dacc18972c1c2f5d054a9a2eb9f528
Hoppo2 <67666971+Hoppo2@users.noreply.github.com>
2022-05-30 08:52:50 +1000

    Create 03.11.2021.py
    Version History

---
+++ src/version_history/03.11.2021.py
@@ -0,0 +1,135 @@
+# Data-set library imports:
+from astroquery.mast import Observations, Catalogs, Tesscut
+
+from astropy.coordinates import SkyCoord
+
+# Data analysis library imports:
+import tensorflow as tf
+import numpy as np
+import sklearn as skl
+from sklearn import linear_model, datasets
+from sklearn.metrics import r2_score, mean_squared_error
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Other Libraries
+import csv
+import os
+
+
+def regression(version: bool = False):
+    if version:
+        print(f"TF Version: {tf.__version__}")
+        print(f"NP Version: {np.__version__}")
+        print(f"SKL Version: {skl.__version__}")
+        print(f"PD Version: {pd.__version__}")
+
+
+class Plot:
+    def __init__(self, count: int):
+        self.count = count
+
+    def rs_i(self):
+        x, y = [], []
+        x2, y2 = [], []
+        x3, y3 = [], []
+        x4, y4 = [], []
+        x5, y5 = [], []
+
+        with open("data_sets/COMBO17.csv", "r") as COMBO17:
+            plots = csv.reader(COMBO17, delimiter=',')
+
+            c = 1
+            for row in plots:
+                if c == 1:
+                    c += 1
+                elif c <= self.count and c != 1:
+                    y.append(float(row[5]))
+                    y2.append(float(row[5]))
+                    y3.append(float(row[5]))
+                    y4.append(float(row[5]))
+                    y5.append(float(row[5]))
+
+                    x.append(float(row[55]))
+                    x2.append(float(row[57]))
+                    x3.append(float(row[59]))
+                    x4.append(float(row[61]))
+                    x5.append(float(row[63]))
+
+                    c += 1
+                else:
+                    break
+
+            figure, axis = plt.subplots(3, 2)
+
+            axis[0, 0].scatter(x, y, 1)
+            axis[0, 0].set_title("UFS")
+
+            axis[1, 0].scatter(x2, y2, 1)
+            axis[1, 0].set_title("BFS")
+
+            axis[2, 0].scatter(x3, y3, 1)
+            axis[2, 0].set_title("VFD")
+
+            axis[0, 1].scatter(x4, y4, 1)
+            axis[0, 1].set_title("RFS")
+
+            axis[1, 1].scatter(x5, y5, 1)
+            axis[1, 1].set_title("IFD")
+
+            plt.show()
+
+    def s_i(self):
+        x, y = [], []
+        x2, y2 = [], []
+        x3, y3 = [], []
+        x4, y4 = [], []
+        x5, y5 = [], []
+
+        with open("data_sets/COMBO17.csv", "r") as COMBO17:
+            plots = csv.reader(COMBO17, delimiter=',')
+
+            c = 1
+            for row in plots:
+                if c == 1:
+                    c += 1
+                elif c <= self.count and c != 1:
+                    y.append(float(row[3]))
+                    y2.append(float(row[3]))
+                    y3.append(float(row[3]))
+                    y4.append(float(row[3]))
+                    y5.append(float(row[3]))
+
+                    x.append(float(row[55]))
+                    x2.append(float(row[57]))
+                    x3.append(float(row[59]))
+                    x4.append(float(row[61]))
+                    x5.append(float(row[63]))
+
+                    c += 1
+                else:
+                    break
+
+            figure, axis = plt.subplots(3, 2)
+
+            axis[0, 0].scatter(x, y, 1)
+            axis[0, 0].set_title("UFS")
+
+            axis[1, 0].scatter(x2, y2, 1)
+            axis[1, 0].set_title("BFS")
+
+            axis[2, 0].scatter(x3, y3, 1)
+            axis[2, 0].set_title("VFD")
+
+            axis[0, 1].scatter(x4, y4, 1)
+            axis[0, 1].set_title("RFS")
+
+            axis[1, 1].scatter(x5, y5, 1)
+            axis[1, 1].set_title("IFD")
+
+            plt.show()
+
+
+if __name__ == "__main__":
+    x = Plot(9997)
+    x.rs_i()
