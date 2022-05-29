6ce05248f91a8755c449dcc3c221876dc9510dfa
Hoppo2 <67666971+Hoppo2@users.noreply.github.com>
2022-05-30 08:59:00 +1000

    Create 03.02.2022.py
    Updated Version History

---
+++ src/version_history/03.02.2022.py
@@ -0,0 +1,80 @@
+from astroquery.mast import Observations, Catalogs, Tesscut, Zcut
+from astropy.coordinates import SkyCoord
+
+import tensorflow as tf
+import sklearn as skl
+from sklearn import linear_model
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.cluster import KMeans
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from yellowbrick.cluster import SilhouetteVisualizer
+
+import csv
+import os
+
+
+class _Data:
+    def __init__(self):
+        self.data = pd.read_csv("data_sets/COMBO17.csv")
+        self.points = np.array(
+            list(zip(self.data["Mcz"], self.data["W420FE"], self.data["W462FE"], self.data["W485FD"],
+                     self.data["W518FE"],
+                     self.data["W571FS"], self.data["W604FE"],
+                     self.data["W646FD"], self.data["W696FE"], self.data["W753FE"], self.data["W815FS"],
+                     self.data["W856FD"], self.data["W914FD"],
+                     self.data["W914FE"])))
+
+
+class Regression(_Data):
+    def __init__(self):
+        _Data.__init__(self)
+
+    def complete_linear(self):
+        pass
+
+    def limited_linear(self):
+        pass
+
+
+class Cluster(_Data):
+    def __init__(self):
+        _Data.__init__(self)
+
+    def model(self, cluster):
+        model = KMeans(n_clusters=cluster)
+        model.fit(self.points)
+
+        return model
+
+    def visualiser(self, model):
+        visualiser = SilhouetteVisualizer(model, colors='yellowbrick')
+        visualiser.fit(self.points)
+
+        visualiser.show()
+
+    def three_band(self):
+        xdata = self.data["W462FE"]
+        ydata = self.data["W646FD"]
+        zdata = self.data["W914FD"]
+        cdata = self.data["Mcz"]
+
+        fig = plt.figure(figsize=(4, 4))
+
+        ax = fig.add_subplot(111, projection='3d')
+
+        img = ax.scatter(xdata, ydata, zdata, c=cdata, cmap=plt.hot())
+        fig.colorbar(img)
+        ax.set_xlabel('W462FE')
+        ax.set_ylabel('W646FD')
+        ax.set_zlabel('W914FD')
+
+        plt.show()
+
+
+if __name__ == "__main__":
+    x = Cluster()
+    x.three_band()
+    x.model(x.model(cluster=2).cluster_centers_)
